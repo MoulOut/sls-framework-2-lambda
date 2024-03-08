1ef421a1df7b2592230ef72dde72fe2f6544f17c
MoulOut <mmoulinlima@outlook.com>
2024-03-08 14:37:47 -0300

    feat: :sparkles: add: functions to simulate action

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+module.exports.simulandoUploadCsv = async (event) => {
+  try {
+    console.log('Simule aqui o upload do CSV');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Simulando upload de CSV' }),
+      headers: { 'Content-Type': 'application/json' },
+    };
+  } catch (error) {
+    return {
+      statusCode: error.statusCode || 500,
+      body: JSON.stringify(error),
+    };
+  }
+};
+
 module.exports.handler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
+        message: 'Go Serverless v3.0! Your function executed successfully!',
         input: event,
       },
       null,
