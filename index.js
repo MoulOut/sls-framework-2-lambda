7977baf4fd095be56520790c9bbb33321316d09a
MoulOut <mmoulinlima@outlook.com>
2024-03-08 16:39:22 -0300

    feat: :sparkles: add: function to locally manipulate s3 bucket

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+
+async function fazUploadNoBucket() {
+  const client = new S3Client({
+    forcePathStyle: true,
+    credentials: {
+      accessKeyId: 'S3RVER',
+      secretAccessKey: 'S3RVER',
+    },
+    endpoint: 'http://localhost:4569',
+  });
+
+  const comandoUpload = new PutObjectCommand({
+    Bucket: 'alunos-csv-local',
+    Key: 'test.csv',
+    Body: Buffer.from('12345'),
+  });
+
+  await client.send(comandoUpload);
+}
+
 module.exports.simulandoUploadCsv = async (event) => {
   try {
-    console.log('Simule aqui o upload do CSV');
+    await fazUploadNoBucket();
 
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Simulando upload de CSV' }),
-      headers: { 'Content-Type': 'application/json' },
     };
   } catch (error) {
     return {
       statusCode: error.statusCode || 500,
       body: JSON.stringify(error),
+      headers: { 'Content-Type': 'application/json' },
     };
   }
 };
 
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+module.exports.cadastrarAlunos = async (event) => {
+  console.log('Funcao lambda executada a partir do bucket s3');
 };
