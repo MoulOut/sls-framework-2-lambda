fb5d05ee296263dc6457501917b66b6e31f283a2
MoulOut <mmoulinlima@outlook.com>
2024-03-11 11:33:26 -0300

    feat: :sparkles: add: config to deploy function

---
+++ cadastro_batch/aws/index.js
@@ -0,0 +1,43 @@
+const { S3Client } = require('@aws-sdk/client-s3');
+const { cadastrarAlunosNoBd } = require('../cadastrarAlunosNoBd');
+const { converteDadosCsv } = require('../converteDadosCsv');
+
+async function obtemDadosDoCsv(bucket, key) {
+  const client = new S3Client({});
+
+  const command = new GetObjectCommand({
+    Bucket: bucket,
+    Key: key,
+  });
+
+  const response = await client.send(command);
+  const csvData = response.Body.transformToString('utf-8');
+
+  return csvData;
+}
+
+module.exports = {
+  obtemDadosDoCsv,
+  fazUploadNoBucket,
+};
+
+module.exports.cadastrarAlunos = async (event) => {
+  try {
+    const eventoS3 = event.Records[0].s3;
+
+    const bucketName = eventoS3.bucket.name;
+    const keyBucket = decodeURIComponent(
+      eventoS3.object.key.replace(/\+/g, ' ')
+    );
+
+    const fileData = await obtemDadosDoCsv(bucketName, keyBucket);
+
+    const alunos = await converteDadosCsv(fileData);
+
+    await cadastrarAlunosNoBd(alunos);
+
+    console.log('Cadastro dos alunos realizado com sucesso.');
+  } catch (error) {
+    console.log(error);
+  }
+};
